1d4bb86887e970c7b08a0de3fc2275cf4ab02c2f
JakubMrozek <jakub.mrozek@gmail.com>
2013-03-06 11:53:50 +0100

    Remove all templates

--- templates/typescríipt/filter.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('<%= _.camelize(appname) %>App')
-  .filter('<%= _.camelize(name) %>', function () {
-    return function (input: string) {
-      return '<%= _.camelize(name) %> filter: ' + input;
-    };
-  });
